0b07be9d9539878db3c426ff6040107714920e5f
aviveli <78605450+aviveli@users.noreply.github.com>
2024-11-24 22:08:15 +0200

    Update Settlement.cpp
    settlement im hahachim
    
    Co-Authored-By: arikshvarts <104718686+arikshvarts@users.noreply.github.com>

--- src/Settlement.cpp
+++ src/Settlement.cpp
@@ -1,17 +1,14 @@
-#include "Settlement.h"
+#include <iostream>
+#include "../include/Settlement.h"
 
-Settlement::Settlement(const string &name, SettlementType type):name(name)
-{
-    this->type=type;
-}
-const string &Settlement::getName() const
-{
-    return name;
-}
-
-SettlementType Settlement::getType() const
-{
-    return SettlementType();
-}
-
-
+    Settlement :: Settlement(const string &name, SettlementType type) : name(name), type(type){}
+    const string &Settlement :: getName() const{
+        return name;
+    }
+    SettlementType Settlement ::  getType() const{
+        return type;
+    }
+    const string Settlement ::  toString() const  {
+        string types[3] = {"Village","City", "Metropolin"};
+        return "Name: " + getName() + "     Type: " + types[int(type)];
+    }   
