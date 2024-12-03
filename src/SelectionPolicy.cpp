f62710f042542192b6af7d402e9b334933f1acd6
aviveli <78605450+aviveli@users.noreply.github.com>
2024-12-03 20:57:53 +0200

    .

--- src/SelectionPolicy.cpp
+++ src/SelectionPolicy.cpp
@@ -4,9 +4,77 @@
 using namespace std;
 
 
+<<<<<<< Updated upstream
 NaiveSelection ::NaiveSelection() : lastSelectedIndex(-1){} //initalize last selected index to -1 because after the first selection, +1 to lastselected brings to 0 
 const FacilityType& NaiveSelection :: selectFacility(const vector<FacilityType>& facilitiesOptions) {
     lastSelectedIndex = (lastSelectedIndex + 1) % facilitiesOptions.size();
+=======
+const FacilityType &BalancedSelection::selectFacility(const vector<FacilityType> &facilitiesOptions)
+{
+    int counter=0;
+    int max_Facility_index=-999;
+    int currSmallest = std::numeric_limits<double>::max();
+    for (FacilityType instance : facilitiesOptions)
+    {
+        double ec=EconomyScore+instance.getEconomyScore();
+        double env=EnvironmentScore+instance.getEnvironmentScore();
+        double lifeQual=LifeQualityScore+instance.getLifeQualityScore();
+        if(max({ec,env,lifeQual})-min({ec,env,lifeQual})<currSmallest)
+        {
+        currSmallest=max({ec,env,lifeQual})-min({ec,env,lifeQual});
+        max_Facility_index=counter;
+        }
+        counter++;
+        
+    }
+    return facilitiesOptions[max_Facility_index];
+    }
+
+const string BalancedSelection::toString() const
+{
+    std::ostringstream oss;
+    oss << "LifeQualityScore: " << LifeQualityScore << ", EconomyScore: " << EconomyScore<< ", EnvironmentScore: " << EnvironmentScore;
+    return oss.str();    
+}
+
+BalancedSelection * BalancedSelection::clone() const
+{
+    return new BalancedSelection(*this); // Use copy constructor to create a deep copy
+}
+
+SustainabilitySelection::SustainabilitySelection():lastSelectedIndex(-1)
+{}
+
+const FacilityType &SustainabilitySelection::selectFacility(const vector<FacilityType> &facilitiesOptions)
+{
+    for (size_t i = lastSelectedIndex; i < facilitiesOptions.size(); ++i) {
+    
+        lastSelectedIndex=(lastSelectedIndex+1)%(facilitiesOptions.size());
+        if (facilitiesOptions[lastSelectedIndex].getCategory()==FacilityCategory::ENVIRONMENT)
+        {
+        return facilitiesOptions[lastSelectedIndex];
+        }
+    }
+
+}
+
+const string SustainabilitySelection::toString() const
+{
+cout<<"Sus";
+}
+
+SustainabilitySelection *SustainabilitySelection::clone() const
+{
+    return new SustainabilitySelection(*this); // Use copy constructor to create a deep copy
+}
+
+NaiveSelection::NaiveSelection():lastSelectedIndex(-1)
+{}
+
+const FacilityType &NaiveSelection::selectFacility(const vector<FacilityType> &facilitiesOptions)
+{
+    lastSelectedIndex=(lastSelectedIndex+1)%(facilitiesOptions.size());
+>>>>>>> Stashed changes
     return facilitiesOptions[lastSelectedIndex];
 }
 const string NaiveSelection :: toString() const{
--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ int main(int argc, char** argv){
     // 	delete backup;
     // 	backup = nullptr;
     // }
+<<<<<<< Updated upstream
     string name = "avivEli";
     string settlementName = "BeitarJerusalem";
     FacilityCategory category = FacilityCategory::ECONOMY;
@@ -36,4 +37,74 @@ int main(int argc, char** argv){
       cout << "aaab" << endl;
     Settlement s1("Beer Sheva", SettlementType(1));
     cout << s1.toString() << endl;
+=======
+    // string name = "avivEli";
+    // string settlementName = "BeitarJerusalem";
+    // FacilityCategory category = FacilityCategory::ECONOMY;
+    // SettlementType type = SettlementType::CITY;
+
+    // cout << "aviv eli goat";
+
+    // cout << "aviv eli goat";
+    // int i=0;
+    // Settlement set(name,type);
+    // Facility fac(name,settlementName,category,55,55,5,55);
+    // cout << fac.getCost();
+
+    // cout << fac.getLifeQualityScore();
+    // Simulation simulation("config_file.txt");
+    // simulation.printInitialState();
+    // simulation.start();
+
+   const string s = "Set_Name";
+    Settlement settlement = Settlement(s, SettlementType::METROPOLIS);
+    cout << settlement.getName() << endl;
+
+    // Create FacilityTypes
+    FacilityType facilityT = FacilityType("setty", FacilityCategory::ECONOMY, 20, 3, 7, 5);  // 4,8,7=3
+    FacilityType facilityT2 = FacilityType("setty2", FacilityCategory::ENVIRONMENT, 3, 6, 4, 6);  // 7,5,8=3
+    FacilityType facilityT3 = FacilityType("setty3", FacilityCategory::ENVIRONMENT, 8, 2, 3, 0);  // 3,4,2=2
+    FacilityType facilityT4 = FacilityType("setty4", FacilityCategory::ECONOMY, 20, 5, 5, 4);  // 3,4,6=3
+    FacilityType facilityT5 = FacilityType("setty5", FacilityCategory::ENVIRONMENT, 20, 4, 7, 3);  // 5,8,5=3
+    FacilityType facilityT6 = FacilityType("setty6", FacilityCategory::ECONOMY, 20, 4, 4, 4);  // 5,5,6=1
+
+    // // Create Facilities
+    Facility facility = Facility("Fac_Name", settlement.getName(),
+        FacilityCategory::LIFE_QUALITY, 10, 3, 5, 2);
+    Facility facility2 = Facility("def", settlement.getName(),
+        FacilityCategory::LIFE_QUALITY, 10, 3, 5, 2);
+    cout << facility.toString() << endl;
+
+    // // Create vector of FacilityTypes
+    vector<FacilityType> facilitiesOptions = { facilityT, facilityT2, facilityT3,
+        facilityT4, facilityT5, facilityT6 };
+
+    // Create BalancedSelection policy
+    BalancedSelection bs = BalancedSelection(1, 1, 2);
+    cout << bs.selectFacility(facilitiesOptions).getName() << endl;
+
+
+    // // Create Plan and execute steps
+    Plan p1 = Plan(100, settlement, new NaiveSelection(), facilitiesOptions);
+    cout << p1.toString() << endl;
+    cout << p1.getEconomyScore() << endl;
+    // cout << "step 1:" << endl;
+    // p1.step();
+    // cout << "step 2:" << endl;
+    // p1.step();
+    // p1.setSelectionPolicy(new SustainabilitySelection());
+    // cout << "step 3:" << endl;
+    // p1.step();
+    // cout << "step 4:" << endl;
+    // p1.step();
+    // // cout << "step 5"<< endl;
+    // p1.step();
+    // cout << "step 6:" << endl;
+    // p1.step();
+    // Settlement set1 = Settlement("metro", SettlementType::METROPOLIS);
+    // cout << int(set1.getType());
+    // // cout << set1.toString();
+
+    return 0;
+>>>>>>> Stashed changes
 }
